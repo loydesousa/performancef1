b5e13454c43d535b249cc032fb4c9f69e07b6f66
Loyde Dourado | Python | Pandas | SQL <loydedourado15@gmail.com>
2026-06-27 19:34:09 -0300

    refactor: delete output directory

--- output/main.py
+++
@@ -1,9 +0,0 @@
-from analysis import get_data_csvteams, get_data_csvdrivers
-
-dataframe = get_data_csvteams("data/Formula1_2025Season_RaceResults.csv")
-print(dataframe)
-print("\n")
-
-dataframe = get_data_csvdrivers("data/Formula1_2025Season_RaceResults.csv")
-print(dataframe)
-print("\n")
